7f965c3396eb2da934df5eeae77da1c3db886120
“MarikoOhtsuka” <mariko.ohtsuka@nnh.go.jp>
2021-11-09 13:54:14 +0900

    add test script

--- R/get-visit.R
+++ R/get-visit.R
@@ -3,24 +3,30 @@
 #' @file get-visit.R
 #' @author Mariko Ohtsuka
 #' @date 2021.10.20
-# ------ constants ------
-kInputDirPath <- '/Users/mariko/Library/CloudStorage/Box-Box/Datacenter/Users/ohtsuka/2021/20211021/DS-ALL-2016_cdisc_210812_1835/'
+# ------ settings ------
+kInputDirPath <- '/Users/mariko/Documents/GitHub/SDTM-Central-Monitoring/TEST/temp/'
 kInputFileName <- 'FA.csv'
-kExternalDirPath <- ''
-kExternalFileName <- 'FA.csv'
-kOutputDirpath <- ''
+kExternalDirPath <- ''  # If it is blank, it is treated as the same as the path set in the "kInputDirPath" variable.
+kExternalFileName <- 'visit.csv'
+kOutputDirpath <- ''  # If it is blank, it is treated as the same as the path set in the "kInputDirPath" variable.
+kOutputFileName <- 'output.csv'
+# ------ constants ------
 kColnameVisitnum <- 'visitnum'
 kColnameVisit <- 'visit'
 kVisitListVisitnumCol <- 1
 kVisitListVisitCol <- 2
 # ------ functions ------
+#' @title ReadTargetCsv
+#' @param inputPath The path where the CSV file is located.
+#' @param targetFilename Name of the CSV file.
+#' @return a data frame.
 ReadTargetCsv <- function(inputPath, targetFilename){
   # If the string of the specified file path ends with '\', remove the '\'.
   temp_path <- ifelse(substr(inputPath, nchar(inputPath), nchar(inputPath)) == '/',
                       substr(inputPath, 1, nchar(inputPath) - 1),
                       inputPath)
   # Returns NULL if the file fails to read.
-  res <- tryCatch(read.csv(paste0(temp_path, '/', targetFilename)),
+  res <- tryCatch(read.csv(paste0(temp_path, '/', targetFilename), header=T, as.is=T),
                   error = function(e){
                     return(NULL)
                   },
@@ -51,19 +57,21 @@ if (length(temp_col_idx) > 0){
 }
 # ------ Get visit info ------
 # Extract only the columns named 'visitnum' and 'visit'.
-target_col_idx <- grep(x=colnames(input_external_file), pattern=paste0('^', kColnameVisitnum, '$|^', kColnameVisit, '$'), ignore.case=T)
-input_visitlist <- input_external_file[ , target_col_idx]
-# Match the column name to the FA.
-target_colname <- colnames(input_visitlist)
-target_colname[kVisitListVisitCol] <- raw_colnames_visit
-colnames(input_visitlist) <- target_colname
+target_col_num_idx <- grep(x=colnames(input_external_file), pattern=paste0('^', kColnameVisitnum, '$'), ignore.case=T)
+target_col_idx <- grep(x=colnames(input_external_file), pattern=paste0('^', kColnameVisit, '$'), ignore.case=T)
+input_visitlist <- input_external_file[ , c(target_col_num_idx, target_col_idx)]
 # Remove duplicates and sort in ascending order by 'visitnum'.
 input_visitlist <- unique(input_visitlist)
 input_visitlist <- input_visitlist[order(input_visitlist[ , kVisitListVisitnumCol]), ]
 # ------ Merge dataframes ------
 raw_colnames <- colnames(input_fa)
 by_x_col_idx <- grep(x=raw_colnames, pattern=paste0('^', kColnameVisitnum, '$'), ignore.case=T)
-temp <- merge(input_fa, input_visitlist, by.x=by_x_col_idx, by.y=kVisitListVisitnumCol)
+input_fa$save_order <- 1:nrow(input_fa)
+temp <- merge(input_fa, input_visitlist, by.x=by_x_col_idx, by.y=kVisitListVisitnumCol, all.x=T, sort=F)
+# Restore the rows to their original order.
+temp <- temp[order(temp$save_order), ]
+temp <- temp[ , colnames(temp) != 'save_order']
+row.names(temp) <- 1:nrow(temp)
 # Restore the columns to their original order.
 output_fa <- NULL
 output_colnames <- c(raw_colnames, kColnameVisit)
@@ -76,3 +84,4 @@ for (i in 1:length(output_colnames)){
     output_fa <- temp[from_idx]
   }
 }
+write.table(output_fa, paste0(kOutputDirpath, kOutputFileName), , row.names=F, append=F, sep=',')
---
+++ TEST/run_test.R
@@ -0,0 +1,2 @@
+library(here)
+runTestFile(here("TEST", "test.get-visit.R"))
